5c0d7ec91575088dd7d35a404b9c27c52c650dab
bthompsonevan <bthompson.evan@gmail.com>
2019-12-02 22:00:21 -0800

    Added onClick to XML layout, added swtich statment to onClick method, Added intent to onClick method and passed a successful test string to secondActivity

--- app/src/main/java/com/example/a5_minute_adventures/SecondActivity.java
+++ app/src/main/java/com/example/a5_minute_adventures/SecondActivity.java
@@ -1,6 +1,27 @@
 package com.example.a5_minute_adventures;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import static com.example.a5_minute_adventures.MainActivity.YES_DATA;
+import static com.example.a5_minute_adventures.MainActivity.NO_DATA;
+
 public class SecondActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+        Intent intent = getIntent();
+        String testString1 = intent.getStringExtra(YES_DATA);
+        Toast toast = Toast.makeText(this, testString1, Toast.LENGTH_LONG);
+        toast.show();
+
+    }
+
+
+
 }
