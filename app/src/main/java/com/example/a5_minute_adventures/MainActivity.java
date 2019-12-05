208106edbc91685ddd837265e138024b52b775fe
bthompsonevan <bthompson.evan@gmail.com>
2019-12-04 19:43:16 -0800

    changed to using sharedPreferences, deleted savedinstatncestate and created onpause/onresume

--- app/src/main/java/com/example/a5_minute_adventures/MainActivity.java
+++ app/src/main/java/com/example/a5_minute_adventures/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.a5_minute_adventures;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import java.util.ArrayList;
 
@@ -10,6 +12,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;  // Used for testing in this app.
 
 
 public class MainActivity extends AppCompatActivity
@@ -27,6 +30,13 @@ public class MainActivity extends AppCompatActivity
     //Variables to hold the current area data
     private String yes = "test";
 
+    //Shared Pref variable
+    private SharedPreferences savedValues;
+
+    //Shared Pref callback constants
+    public static final String SAVED_VALUES = "savedValues";
+    public static final String TEXT_BOX = "textBox";
+
     //variables to hold information from the widgets
     TextView questTextBox;
     Button yesButton;
@@ -40,6 +50,8 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        savedValues = getSharedPreferences(SAVED_VALUES,MODE_PRIVATE);
+
         questTextBox = findViewById(R.id.questTextBox);
         yesButton = findViewById(R.id.yesButton);
         noButton = findViewById(R.id.noButton);
@@ -51,10 +63,26 @@ public class MainActivity extends AppCompatActivity
 
         adventureItems = dal.parseXmlFile(ADVENTURE_1);
 
-        //USE THIS FORMAT WHEN PULLING DATA OUT OF THE ARRAYLIST
+        //USE THIS FORMAT WHEN PULLING DATA OUT OF THE ARRAY LIST
         // questTextBox.setText(adventureItems.get(0).getTextBox());
         questTextBox.setText(adventureItems.get(0).getTextBox());
 
+    }
+
+    @Override
+    public void onPause(){
+        // Saving values to be restored
+        Editor editor = savedValues.edit();
+        editor.putString(TEXT_BOX, questTextBox.getText().toString());
+        editor.commit();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        
     }
 
     @Override
@@ -74,9 +102,11 @@ public class MainActivity extends AppCompatActivity
                 break;
 
             case R.id.moveButton:
-
+                questTextBox.setText("Test Screen for saved instance state");
                 break;
         }
 
     }
+
+
 }
--- app/src/main/java/com/example/a5_minute_adventures/SecondActivity.java
+++ app/src/main/java/com/example/a5_minute_adventures/SecondActivity.java
@@ -2,7 +2,9 @@ package com.example.a5_minute_adventures;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,14 +13,20 @@ import static com.example.a5_minute_adventures.MainActivity.NO_DATA;
 
 public class SecondActivity extends AppCompatActivity {
 
+    TextView choiceTextBox;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
+
+        choiceTextBox = findViewById(R.id.choiceTextBox);
 
         Intent intent = getIntent();
         String testString1 = intent.getStringExtra(YES_DATA);
         Toast toast = Toast.makeText(this, testString1, Toast.LENGTH_LONG);
         toast.show();
+
+        choiceTextBox.setText(YES_DATA);
 
     }
 
